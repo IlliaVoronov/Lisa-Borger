35c30eb38bb2b12358162134098ed0647d19f660
Illia Voronov <fleshvoronov2005@gmail.com>
2025-06-27 11:04:29 +0200

    added analitycs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App() {
 
   return (
     <div className="min-h-screen bg-orange-primary text-gray-100 transition-opacity duration-700 font-mono">
+      <Analytics />
       <Navbar />
       <main>
         <Outlet />
